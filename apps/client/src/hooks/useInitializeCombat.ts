984cfca0280963c347f28078abda3fe59e70bd4d
mpm <snaxmiller@gmail.com>
2024-08-07 22:52:58 -0500

    initialize bugfix

--- apps/client/src/hooks/useInitializeCombat.ts
+++ apps/client/src/hooks/useInitializeCombat.ts
@@ -2,10 +2,12 @@ import { faker } from '@faker-js/faker'
 import { TeamId } from '@repo/game/data'
 import { Team, UnitBuilder } from '@repo/game/types'
 import { makeUnit, resolveUnitBuilder } from '@repo/game/utils'
-import { useCombat } from './state'
+import { useActions, useCleanup, useCombat } from './state'
 import { useNavigate } from '@tanstack/react-router'
 
 export function useInitializeCombat() {
+  const actions = useActions()
+  const cleanup = useCleanup()
   const combat = useCombat()
   const navigate = useNavigate()
 
@@ -23,6 +25,8 @@ export function useInitializeCombat() {
       makeUnit(aiTeam.id, faker.person.fullName(), false),
       makeUnit(aiTeam.id, faker.person.fullName(), false),
     ]
+    actions.setQueue(() => [])
+    cleanup.setQueue(() => [])
     combat.initialize({ teams, units, user: userTeam.id })
 
     navigate({

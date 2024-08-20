8b909b420f046c3ad8813b8559d32a1e176a106d
mpm <snaxmiller@gmail.com>
2024-08-20 12:19:55 -0500

    modified:   packages/game/utils/mapBaseStat.ts

--- packages/game/utils/mapBaseStat.ts
+++ packages/game/utils/mapBaseStat.ts
@@ -3,5 +3,5 @@ import { StatKey } from '../types'
 export function mapBaseStat(key: StatKey, base: number, level: number): number {
   const value = (base * level) / 50
   if (key === 'health') return value + level
-  return value
+  return Math.round(value)
 }

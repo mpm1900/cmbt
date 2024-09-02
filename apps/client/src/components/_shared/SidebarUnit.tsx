5df0ba91134dcd823226c5ecac65340f211ba799
mpm <snaxmiller@gmail.com>
2024-09-02 17:04:12 -0500

    better ux

--- apps/client/src/components/_shared/SidebarUnit.tsx
+++ apps/client/src/components/_shared/SidebarUnit.tsx
@@ -8,7 +8,6 @@ import {
   getUnitBase,
   validateModifiers,
 } from '@repo/game/utils'
-import { CgDetailsMore } from 'react-icons/cg'
 import { Button } from '../ui/button'
 import { HoverCard, HoverCardContent, HoverCardTrigger } from '../ui/hover-card'
 import { EditUnitModal } from './EditUnitModal'
@@ -44,58 +43,50 @@ export function SidebarUnit(props: SidebarUnitProps) {
   const nonStatusModifiers = mods.filter((m) => !m.statusId)
   const statuses = getStatusesFromModifiers(mods)
   return (
-    <div className="rounded hover:bg-slate-800 flex flex-row space-x-2 items-center">
-      <HoverCard openDelay={300} closeDelay={100}>
-        <HoverCardTrigger asChild>
-          <div className="p-4 pr-0 flex-1">
-            <div className="flex-1 space-y-2">
-              <div className="flex justify-between items-center">
-                <span className="space-x-2">
-                  <span className="font-black">
-                    <span className="text-sm text-muted-foreground font-thin">
-                      Lv.
+    <EditUnitModal
+      unit={props.unit}
+      onChange={(changes) =>
+        game.updateUnits((u) => (u.id === unit.id ? changes : u))
+      }
+    >
+      <Button variant="ghost" className="w-full h-full p-0">
+        <HoverCard openDelay={300} closeDelay={100}>
+          <HoverCardTrigger asChild>
+            <div className="flex-1 p-4">
+              <div className="flex-1 space-y-2">
+                <div className="flex justify-between items-center">
+                  <span className="space-x-2">
+                    <span className="font-black">
+                      <span className="text-sm text-muted-foreground font-thin">
+                        Lv.
+                      </span>
+                      {unit.level}
                     </span>
-                    {unit.level}
+                    <span>{unit.name}</span>
                   </span>
-                  <span>{unit.name}</span>
-                </span>
-                <span className="text-xs text-muted-foreground">
-                  {base?.name}
-                </span>
-              </div>
+                  <span className="text-xs text-muted-foreground">
+                    {base?.name}
+                  </span>
+                </div>
 
-              <HealthBar unit={unit} initial={ratio} />
-              <UnitModifiers
-                modifiers={nonStatusModifiers}
-                statuses={statuses}
-              />
+                <HealthBar unit={unit} initial={ratio} />
+                <UnitModifiers
+                  modifiers={nonStatusModifiers}
+                  statuses={statuses}
+                />
+              </div>
             </div>
-          </div>
-        </HoverCardTrigger>
-        <HoverCardContent
-          side="left"
-          className="w-full"
-          collisionPadding={32}
-          sideOffset={16}
-        >
-          <UnitStats unit={unit} comp={props.unit} />
-        </HoverCardContent>
-      </HoverCard>
-      <div className="p-2">
-        <EditUnitModal
-          unit={props.unit}
-          onChange={(changes) =>
-            game.updateUnits((u) => (u.id === unit.id ? changes : u))
-          }
-        >
-          <Button
-            variant="ghost"
-            className="h-full opacity-40 hover:opacity-100 p-2"
+          </HoverCardTrigger>
+          <HoverCardContent
+            side="left"
+            className="w-full"
+            collisionPadding={32}
+            sideOffset={16}
           >
-            <CgDetailsMore size={28} />
-          </Button>
-        </EditUnitModal>
-      </div>
-    </div>
+            <UnitStats unit={unit} comp={props.unit} />
+          </HoverCardContent>
+        </HoverCard>
+      </Button>
+    </EditUnitModal>
   )
 }

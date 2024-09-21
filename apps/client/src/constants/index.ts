ea74b89ad265cd9ca9165c0f3e1598458fdf5c79
mpm <snaxmiller@gmail.com>
2024-09-21 01:01:53 -0500

    slower speed is better

--- apps/client/src/constants/index.ts
+++ apps/client/src/constants/index.ts
@@ -1,5 +1,5 @@
 // export const MAX_ACTIVE_UNITS_COUNT = 2
 export const MAX_UNITS_PER_TEAM = 4
-export const GAME_SPEED = 800
+export const GAME_SPEED = 1200
 
 export * from './routeTransitionProps'
